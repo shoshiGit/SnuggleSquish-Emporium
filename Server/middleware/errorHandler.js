9091a74e4a00d8d8e1edc47a9859f3d8e63dafb3
Shoshi Wolpe <158214649+shoshiGit@users.noreply.github.com>
2024-09-16 15:24:29 +0300

    Delete Server directory

--- Server/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-export const errorHandling=(err, req, res, next) => {
-    console.error(err.stack);
-    let statusCode = err.status || 500;
-    let message = err.message || "מצטערים התרחשה שגיאה בשרת"
-    res.status(statusCode).send({status: statusCode, message: message});
-}
--- Server/models/cart.js
+++
@@ -1,9 +0,0 @@
-import { Schema } from "mongoose";
-
-const cartSchema = new Schema({
-    username: {type:String,},
-    img:{type:String},
-    amount:{type:String},
-    price:{type:Number}
-})
-export const cartModel = mongoose.model('Cart', cartSchema)
